b2c0229a412d8014d379bfd5f0854faec435c788
TienNguyen <6051071122@st.utc2.edu.vn>
2023-06-18 03:26:58 +0700

    upload code

--- client/src/_mock/account.js
+++ client/src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: "Jaydon Frankie",
+  displayName: "NVT",
   email: "demo@minimals.cc",
   photoURL:
     "https://i.pinimg.com/474x/ca/28/c7/ca28c7b21c11ae90bd0ad8be56271ef5.jpg",
